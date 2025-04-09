8687472f774ff4d8ee493a4c1e477acdc1290274
Taiyyab Anwar Makandar <taiyyabmakandar@gmail.com>
2025-04-09 16:40:14 +0530

    backend remove

--- Backend/router/contact-router.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const contactForm = require("../controller/contact-controllers");
-
-router.route("/contact").post(contactForm);
-module.exports = router;
